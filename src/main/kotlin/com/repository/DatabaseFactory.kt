97106b833d45acb624bfd7e3f50680cdc0ad8b18
PrathameshKumbhar291113 <prathameshkumbhar291113@gmail.com>
2024-02-13 00:43:48 +0530

    [Code Changes]: added the url of database to DatabaseFactory.kt

--- src/main/kotlin/com/repository/DatabaseFactory.kt
+++ src/main/kotlin/com/repository/DatabaseFactory.kt
@@ -9,6 +9,7 @@ import kotlinx.coroutines.withContext
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
+import java.net.URI
 
 object DatabaseFactory {
 
@@ -24,7 +25,14 @@ object DatabaseFactory {
     private fun hikari(): HikariDataSource {
         val config = HikariConfig()
         config.driverClassName = System.getenv("JDBC_DRIVER") // 1
-        config.jdbcUrl = System.getenv("DATABASE_URL") // 2
+//        config.jdbcUrl = System.getenv("DATABASE_URL")
+        val uri = URI(System.getenv("DATABASE_URL"))
+        val username = uri.userInfo.split(":").toTypedArray()[0]
+        val password = uri.userInfo.split(":").toTypedArray()[1]
+
+        config.jdbcUrl =
+            "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path + "?sslmode=require" + "&user=$username&password=$password"
+
         config.maximumPoolSize = 3
         config.isAutoCommit = false
         config.transactionIsolation = "TRANSACTION_REPEATABLE_READ"
